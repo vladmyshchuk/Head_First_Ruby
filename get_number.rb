2ae2f0659c4c9e6e43b62eb76f4e8dc998e283e3
Vlad Myshchuk <vladmyshchuk@gmail.com>
2020-01-22 20:07:18 -0500

    Initial Commit

---
+++ get_number.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Guess My Number Game
+# Written by: me - Vlad!
+
+puts "Welcome to 'Guess My Number!'"
+print "What's your name? "
+input = gets
+
+name = input.chomp
+
+puts "Welcome, #{name}! "
